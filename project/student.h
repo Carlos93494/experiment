32a85013f6e634716cd458968fedae0be32236cc
Carlos <Carlos93494@outlook.com>
2025-11-20 18:47:53 +0800

    usual commit message

--- project/student.h
+++ project/student.h
@@ -8,11 +8,13 @@ class Student {
 public:
     Student(const std::string& name, int age);
     void display() const;
-    void setName(const std::string& name) { this->name = name; }
+    void setName(const std::string& name) {
+        this->name = name;
+    }
     void setAge(int age) { this->age = age; }
     std::string getName() const { return name; }
     int getAge() const { return age; }
-    
+
 private:
     std::string name;
     int age;
